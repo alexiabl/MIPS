1bd3a2a52193c0823abd904cb8b0ac1710e3da8e
alexiabl <a1b9l9d6>
2018-06-11 18:42:20 -0600

    archivos hilillos

--- MIPS/src/main/java/arquitectura/mips/util/Util.java
+++ MIPS/src/main/java/arquitectura/mips/util/Util.java
@@ -3,7 +3,7 @@ package arquitectura.mips.util;
 /**
  * Created by alexiaborchgrevink on 6/11/18.
  */
-public class util {
+public class Util {
 
     public void leerArchivos() { //e insertar en memoria principal
         //cada linea por 4 que son las instucciones
